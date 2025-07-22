ae7ac81c57ff2aa72c677053c61e4dd566e1f69a
Fabio Papa <fpapa@springhealth.com>
2025-07-21 21:39:17 -0700

    Implement Icon Input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,78 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const SIZES = {
+    small: {
+      "--height": "24px",
+      "--icon-size": 16,
+      "--padding-left": "24px",
+      "--border-bottom": `1px solid ${COLORS.black}`,
+      "--font-size": "14px",
+    },
+    large: {
+      "--height": "36px",
+      "--icon-size": 24,
+      "--padding-left": "36px",
+      "--border-bottom": `2px solid ${COLORS.black}`,
+      "--font-size": "18px",
+    },
+  };
+
+  return (
+    <Wrapper style={SIZES[size]}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper id={icon} size={SIZES[size]["--icon-size"]} />
+      <Input placeholder={placeholder} width={width} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: var(--border-bottom);
+  width: ${(props) => props.width}px;
+  height: var(--height);
+  padding-left: var(--padding-left);
+  color: ${COLORS.gray700};
+  font-size: var(--font-size);
+  font-weight: 700;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &:focus {
+    outline-offset: 2px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+}
+`;
+
+const IconWrapper = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  color: ${COLORS.gray700};
+  pointer-events: none;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default IconInput;
